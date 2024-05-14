d592e510b575503f71c751834204d6aeab664bbe
Giulia <giulia.marcon.6@studenti.unipd.it>
2024-05-14 22:55:32 +0200

    progetto2

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -9,10 +9,5 @@ import java.util.Scanner;
 
 public class App 
 {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter a number");
 
-    int number = myObj.nextInt();
-    myObj.close();
-    RomanPrinter.print(number);
 }
